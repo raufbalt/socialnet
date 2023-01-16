d678197c6527a8dab6c7e29e1fd0951f727f6c0e
raufbalt <baltabaevrauf9@gmail.com>
2023-01-16 23:02:56 +0600

    delete

--- main/settings.py
+++ main/settings.py
@@ -71,7 +71,7 @@ TEMPLATES = [
 
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
